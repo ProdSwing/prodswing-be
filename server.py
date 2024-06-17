b9b21063f2b82fe8fa35b491a0c06c79e1995518
HP <fernandonathanielsutanto@gmail.com>
2024-06-18 01:40:42 +0700

    add sementara walau masih ada salah

--- server.py
+++ server.py
@@ -1,8 +1,12 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_file
 from google.cloud import firestore, storage
 from werkzeug.utils import secure_filename
 from dotenv import load_dotenv
 import os
+import pandas as pd
+import subprocess
+from datetime import datetime, timedelta
+from apscheduler.schedulers.background import BackgroundScheduler
 
 load_dotenv() 
 
@@ -165,6 +169,60 @@ def delete_product_image(id):
         blob.delete()
     return 'Product image deleted', 200
 
+
+def fetch_and_combine_tweets():
+    print("masuk sini 2")
+    twitter_auth_token = '787d75c3955dbd7aee4a4d1e72f916fb34bfa598'
+    listOfProducts = ["tablet", "laptop", "printer"]
+    limit = 75
+    current_date = datetime.now()
+    previous_date = current_date - timedelta(days=1)
+    day = previous_date.day
+    month = previous_date.month
+    year = previous_date.year
+    filename = f'combined_{previous_date.strftime("%Y-%m-%d")}.csv'
+
+    file_exists = False
+
+    for product in listOfProducts:
+        print(product)
+        search_keyword = f'{product} lang:id since:{year}-{month:02d}-{day:02d} until:{year}-{month:02d}-{day+1:02d}'
+        temp_filename = f'temp_{product}.csv'
+        
+        subprocess.run(f'npx --yes tweet-harvest@2.6.1 -o "{temp_filename}" -s "{search_keyword}" --tab "LATEST" -l {limit} --token {twitter_auth_token}', shell=True)
+        
+        if not file_exists:
+            df = pd.read_csv(temp_filename)
+            df.to_csv(filename, index=False)
+            file_exists = True
+        else:
+            df_temp = pd.read_csv(temp_filename)
+            df_combined = pd.read_csv(filename)
+            df_combined = pd.concat([df_combined, df_temp], ignore_index=True)
+            df_combined.to_csv(filename, index=False)
+
+@app.route('/trigger_fetch', methods=['POST'])
+def trigger_fetch():
+    try:
+        print("masuk sini")
+        fetch_and_combine_tweets()
+        return jsonify({"status": "success"}), 200
+    except Exception as e:
+        return jsonify({"status": "error", "message": str(e)}), 500
+
+@app.route('/get_csv', methods=['GET'])
+def get_csv():
+    previous_date = datetime.now() - timedelta(days=1)
+    filename = f'combined_{previous_date.strftime("%Y-%m-%d")}.csv'
+    
+    if os.path.exists(filename):
+        return send_file(filename, as_attachment=True)
+    else:
+        return "File not found", 404
+
 if __name__ == '__main__':
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(trigger_fetch, 'cron', hour=1, minute=35)
+    scheduler.start()
     port = int(os.getenv('PORT', 3000))
     app.run(host='0.0.0.0', port=port)
